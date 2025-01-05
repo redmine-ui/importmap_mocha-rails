96fb1fc187b7e4ae21d14fe10d439c4fda387d2a
Takashi Kato <tohosaku@users.osdn.me>
2025-01-05 14:25:48 +0900

    Update @mswjs/interceptors to v0.37.5

--- vendor/javascripts/@mswjs--interceptors.js
+++ vendor/javascripts/@mswjs--interceptors.js
@@ -752,19 +752,85 @@ function decodeBuffer(buffer, encoding) {
   return decoder.decode(buffer);
 }
 
-// src/utils/responseUtils.ts
-var RESPONSE_STATUS_CODES_WITHOUT_BODY = /* @__PURE__ */ new Set([
-  101,
-  103,
-  204,
-  205,
-  304
-]);
-function isResponseWithoutBody(status) {
-  return RESPONSE_STATUS_CODES_WITHOUT_BODY.has(status);
-}
+// src/utils/fetchUtils.ts
+var FetchResponse = class _FetchResponse extends Response {
+  static {
+    /**
+     * Response status codes for responses that cannot have body.
+     * @see https://fetch.spec.whatwg.org/#statuses
+     */
+    this.STATUS_CODES_WITHOUT_BODY = [101, 103, 204, 205, 304];
+  }
+  static {
+    this.STATUS_CODES_WITH_REDIRECT = [301, 302, 303, 307, 308];
+  }
+  static isConfigurableStatusCode(status) {
+    return status >= 200 && status <= 599;
+  }
+  static isRedirectResponse(status) {
+    return _FetchResponse.STATUS_CODES_WITH_REDIRECT.includes(status);
+  }
+  /**
+   * Returns a boolean indicating whether the given response status
+   * code represents a response that can have a body.
+   */
+  static isResponseWithBody(status) {
+    return !_FetchResponse.STATUS_CODES_WITHOUT_BODY.includes(status);
+  }
+  static setUrl(url, response) {
+    if (!url) {
+      return;
+    }
+    if (response.url != "") {
+      return;
+    }
+    Object.defineProperty(response, "url", {
+      value: url,
+      enumerable: true,
+      configurable: true,
+      writable: false
+    });
+  }
+  /**
+   * Parses the given raw HTTP headers into a Fetch API `Headers` instance.
+   */
+  static parseRawHeaders(rawHeaders) {
+    const headers = new Headers();
+    for (let line = 0; line < rawHeaders.length; line += 2) {
+      headers.append(rawHeaders[line], rawHeaders[line + 1]);
+    }
+    return headers;
+  }
+  constructor(body, init = {}) {
+    const status = init.status ?? 200;
+    const safeStatus = _FetchResponse.isConfigurableStatusCode(status) ? status : 200;
+    const finalBody = _FetchResponse.isResponseWithBody(status) ? body : null;
+    super(finalBody, {
+      ...init,
+      status: safeStatus
+    });
+    if (status !== safeStatus) {
+      const stateSymbol = Object.getOwnPropertySymbols(this).find(
+        (symbol) => symbol.description === "state"
+      );
+      if (stateSymbol) {
+        const state = Reflect.get(this, stateSymbol);
+        Reflect.set(state, "status", status);
+      } else {
+        Object.defineProperty(this, "status", {
+          value: status,
+          enumerable: true,
+          configurable: true,
+          writable: false
+        });
+      }
+    }
+    _FetchResponse.setUrl(init.url, this);
+  }
+};
 export {
   BatchInterceptor,
+  FetchResponse,
   INTERNAL_REQUEST_ID_HEADER_NAME,
   IS_PATCHED_MODULE,
   Interceptor,
@@ -774,6 +840,5 @@ export {
   deleteGlobalSymbol,
   encodeBuffer,
   getCleanUrl,
-  getGlobalSymbol,
-  isResponseWithoutBody
+  getGlobalSymbol
 };
